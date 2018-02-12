f805c80a7438bcdc0c5a0830b52f232872bcd598
Junshu Liu <jliu237@asu.edu>
2018-02-12 09:19:11 -0700

    update

--- A2/handout/server.js
+++ A2/handout/server.js
@@ -10,15 +10,13 @@ app.get("/", function (req, res) {
     res.redirect("index.html");
 });
 
-var callback = function (req, res) { // req -> request object; res -> response object
-  var eq = req.url.replace("/","");;
-
-  if(eq.indexOf("favicon") >=0 ) return;
+app.get("/eval", function (req, res) {
+  var eq = req.query.eq;
 
   var r = eq + " = " + eval(eq) + "\n";
    res.writeHead(200, {'Content-Type': 'text/plain'}); // send response header
    res.end(r); // send response body
-}
+});
 
 app.use(methodOverride());
 app.use(bodyParser());
