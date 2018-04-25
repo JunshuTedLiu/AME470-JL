a818fda55b3fdf6e3e9b09b270e87e2b497d0184
Junshu Liu <jliu237@asu.edu>
2018-04-25 13:06:36 -0700

    update a7

--- A7/handout/server.js
+++ A7/handout/server.js
@@ -11,6 +11,33 @@ var Client = require('node-rest-client').Client;
 var MS = require("mongoskin");
 var db = MS.db("mongodb://52.53.189.231:27017/ame470");
 
+//s3
+var fs = require('fs');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./credentials.json');
+var s3 = new AWS.S3();
+
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+
+app.use(methodOverride());
+//app.use(bodyParser());
+app.use(require('connect').bodyParser());
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
 
 app.get("/", function (req, res) {
     res.redirect("index.html");
@@ -81,7 +108,44 @@ app.get("/deleteImg", function (req, res) {
   });
 });
 
+app.post('/uploadImage', function(req, res){
+    var intname = req.body.fileInput;
+    var s3Path = '/' + intname;
+    var buf = new Buffer(req.body.data.replace(/^data:image\/\w+;base64,/, ""),'base64');
+    var params = {
+        Bucket:'ame470jl',
+        ACL:'public-read',
+        Key:intname,
+        Body: buf,
+        ServerSideEncryption : 'AES256'
+    };
+    s3.putObject(params, function(err, data) {
+        console.log(err);
+        res.end("success");
+    });
+});
 
+app.post('/uploadFile', function(req, res){
+    var intname = req.body.fileInput;
+    var filename = req.files.input.name;
+    var fileType =  req.files.input.type;
+    var tmpPath = req.files.input.path;
+    var s3Path = '/' + intname;
+    
+    fs.readFile(tmpPath, function (err, data) {
+        var params = {
+            Bucket:'ame470jl',
+            ACL:'public-read',
+            Key:intname,
+            Body: data,
+            ServerSideEncryption : 'AES256'
+        };
+        s3.putObject(params, function(err, data) {
+            res.end("success");
+            console.log(err);
+        });
+    });
+  });
 
 app.use(methodOverride());
 app.use(bodyParser());
